8aedda9e417b716998578ac5eb5515e35e0ed160
dobai <Gabor.Dobai@garmin.com>
2017-07-07 14:16:33 +0300

    Chapter 01 - Basic Syntax
    Solve Using stdout exercise.

---
+++ CppEssT/ExerciseFiles/Chap01/hello-cout.cpp
@@ -0,0 +1,10 @@
+// hello-cout.cpp by Gabor
+// Using stdout
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) 
+{
+	cout << "Hello, World! " <<  2 * 7 << " another string" << endl;
+	return 0;
+}
