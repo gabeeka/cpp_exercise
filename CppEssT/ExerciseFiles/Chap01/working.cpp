82b07d8c076bc8063540f825728627bda3e5fda3
dobai <Gabor.Dobai@garmin.com>
2017-07-07 11:46:38 +0300

    Chapter 01 -Basic Syntax
    Solve Pointers and References exercises.

--- CppEssT/ExerciseFiles/Chap01/working.cpp
+++ CppEssT/ExerciseFiles/Chap01/working.cpp
@@ -1,12 +1,37 @@
 // working.cpp by Gabor
+// Pointers and References exercises
 #include <cstdio>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	int i = 7;
-	i = 47;
-	printf("The value is %d\n", i);
+	int x = 7;
+	int *ip = &x;
+	int &y = x;
+	printf("The value of x is %d\n", x);
+	printf("The value of *ip is %d\n", *ip);
+	printf("The value of y is %d\n\n", y);
+
+	x = 42;
+	printf("The value of x is %d\n", x);
+	printf("The value of *ip is %d\n", *ip);
+	printf("The value of y is %d\n\n", y);
+
+	y = 73;
+	printf("The value of x is %d\n", x);
+	printf("The value of *ip is %d\n", *ip);
+	printf("The value of y is %d\n\n", y);
+
+	int z = 149;
+	ip = &z;
+	printf("The value of x is %d\n", x);
+	printf("The value of *ip is %d\n", *ip);
+	printf("The value of y is %d\n\n", y);
+
+	y = z;
+	printf("The value of x is %d\n", x);
+	printf("The value of *ip is %d\n", *ip);
+	printf("The value of y is %d\n\n", y);
 
 	return 0;
 }
