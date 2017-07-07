b3767196a57b155d1e630a81122754ac8e814ea8
dobai <Gabor.Dobai@garmin.com>
2017-07-07 17:34:32 +0300

    Chapter 02 - Defining Functions
    Solve Using recursion exercise.

---
+++ CppEssT/ExerciseFiles/Chap02/recursive-factorial.cpp
@@ -0,0 +1,14 @@
+// recursive-factorial.cpp by Gabor
+#include <cstdio>
+using namespace std;
+
+unsigned long int factorial(unsigned long int n) {
+	if (n == 1) return 1;
+	return factorial(n - 1) * n;
+}
+
+int main(int argc, char ** argv) {
+	unsigned long int n = 5;
+	printf("Factorial of %ld is %ld\n", n, factorial(n));
+	return 0;
+}
