0b8ebaf8a3cd700a43e377a9005cad23018f5671
dobai <Gabor.Dobai@garmin.com>
2017-07-07 09:54:27 +0300

    Chapter 01 - Basic Syntax
    Solve Introduction to basic syntax, Anatomy of a C++ program, Statements and
    expressions exercises.

---
+++ CppEssT/ExerciseFiles/Chap01/hello.cpp
@@ -0,0 +1,13 @@
+// hello.cpp by Gabor
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	puts("Hello, World!");
+	//printf("Hello, World!\n");
+	//cout << "Hello, World!" << endl;
+	//cout << "Hello, World!\n" << endl;
+	return 0;
+}
---
+++ CppEssT/ExerciseFiles/Chap01/working.cpp
@@ -0,0 +1,11 @@
+// working.cpp by Gabor
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char **argv) 
+{
+	int x;
+	x = 42;
+	printf("x is %d\n", x = 42 * (12 + 14));
+	return 0;
+}
