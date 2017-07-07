8bb147ee745cf00203f65954ceea9396d4bcf717
dobai <Gabor.Dobai@garmin.com>
2017-07-07 13:32:26 +0300

    Chapter 01 - Basic Syntax
    Solve Looping with while and do exercise.

--- CppEssT/ExerciseFiles/Chap01/working.cpp
+++ CppEssT/ExerciseFiles/Chap01/working.cpp
@@ -1,38 +1,23 @@
 // working.cpp by Gabor
-// The branching conditional
+// Looping with while and do
 #include <cstdio>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	const int iONE = 1;
-	const int iTWO = 2;
-	const int iTWENTY = 20;
-	const int iTHREE = 3;
-	const int iFOUR = 4;
+	int a[] = { 1, 2, 3, 4, 5 };
+	int i = 0;
 
-	int x = 20;
+	// while
+	//while (i < 5) {
+	//	printf("element %d. is %d\n", i, a[i]);
+	//	++i;
+	//}
 
-	switch (x)
-	{
-	case iONE:
-		puts("one");
-		break;
-	case iTWO:
-	case iTWENTY:
-		puts("two or twenty");
-		break;
-	case iTHREE:
-		puts("three");
-		break;
-	case iFOUR:
-		puts("four");
-		break;
-
-	default:
-		puts("default");
-		break;
-	}
-
+	// do while
+	do {
+		printf("element %d. is %d\n", i, a[i]);
+		++i;
+	} while (i < 5);
 	return 0;
 }
