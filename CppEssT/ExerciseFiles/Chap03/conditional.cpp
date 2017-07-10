ad63d453ea539110d209594adef9e184519184be
dobai <Gabor.Dobai@garmin.com>
2017-07-10 15:01:11 +0300

    Chapter 03 - The Preprocessor
    Solve Conditional compilation exercise.

---
+++ CppEssT/ExerciseFiles/Chap03/conditional.cpp
@@ -0,0 +1,10 @@
+// conditional.cpp Gabor
+#include <cstdio>
+//#define FOO
+#include "conditional.h"
+
+int main(int argc, char **argv)
+{
+	printf("Number is %d\n", NUMBER);
+	return 0;
+}
---
+++ CppEssT/ExerciseFiles/Chap03/conditional.h
@@ -0,0 +1,15 @@
+// conditional.h by Gabor
+#ifndef CONDITIONAL_H_
+#define CONDITIONAL_H_
+
+#if defined(FOO)
+
+#define NUMBER 47
+
+#else
+
+#define NUMBER 2
+
+#endif
+
+#endif /* CONDITIONAL_H_ */
