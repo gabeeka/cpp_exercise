d5f3039ae826ae3430d41e89d61695d4c82445c4
dobai <Gabor.Dobai@garmin.com>
2017-07-10 16:13:01 +0300

    Chapter 03 - The Preprocessor
    Solve Macros caveats exercise.

--- CppEssT/ExerciseFiles/Chap03/working.cpp
+++ CppEssT/ExerciseFiles/Chap03/working.cpp
@@ -1,16 +1,22 @@
 // working.cpp by Gabor
-// Defining macros
+// Macro caveats
 
 #include <cstdio>
 using namespace std;
 
-#define MACRO(a, b) ( a * b )
 #define MAX(a, b) ( a > b ? a : b )
+
+int increment() {
+	static int i = 42;
+	i += 5;
+	printf("returning: %d\n", i);
+	return i;
+}
 
 int main(int argc, char **argv)
 {
-	int ia = 5;
-	int ib = 7;
-	printf("value is %d\n", MAX(ia, ib));
+	int x = 50;
+	printf("max of %d and %d is: %d\n", x, increment(), MAX(x, increment()));
+	printf("max of %d and %d is: %d\n", x, increment(), MAX(x, increment()));
 	return 0;
 }
