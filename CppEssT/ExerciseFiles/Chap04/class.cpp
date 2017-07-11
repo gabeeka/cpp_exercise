5d5b68598cd203489237bd71af2476d2d83bdf42
dobai <Gabor.Dobai@garmin.com>
2017-07-11 12:52:09 +0300

    Chapter 04 - Classes and Objects
    Solve Pointing to current object with *this exercise.

--- CppEssT/ExerciseFiles/Chap04/class.cpp
+++ CppEssT/ExerciseFiles/Chap04/class.cpp
@@ -1,32 +1,33 @@
-// class.cpp by Gabor
-// Defining a class
+// class.cpp
+// Pointing to current object with *this
 
 #include <cstdio>
 using namespace std;
 
 // a very simple class
 class Class1 {
-private:
-	int i;
+	int i = 0;
 public:
-	void setValue(const int value);
-	int getValue() const;
+	void setvalue(const int value) { i = value; }
+	int getvalue() const;
 };
 
-void Class1::setValue(const int value) {
-	i = value;
-}
-
-int Class1::getValue() const {
+int Class1::getvalue() const {
+	printf("getvalue(): this is %p\n", this);
 	return i;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	int i = 47;
 	Class1 object1;
+	Class1 object2;
 
-	object1.setValue(i);
-	printf("value is %d\n", object1.getValue());
-
+	object1.setvalue(i);
+	object2.setvalue(i);
+	printf("values is %d\n", object1.getvalue());
+	printf("address of object1 is %p\n", &object1);
+	printf("values is %d\n", object2.getvalue());
+	printf("address of object1 is %p\n", &object2);
 	return 0;
 }
